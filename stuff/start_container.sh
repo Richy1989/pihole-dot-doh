a5e5c00de4aad99a89ae7e502b61e76fe3786ee0
Richard Leopold <richy.leopold@outlook.com>
2022-11-22 20:39:57 +0100

    added test startup script

---
+++ stuff/start_container.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+interface='eth0' #'br0'
+ipAddress='192.168.0.10'
+
+pihole_volume='./vpihole' #'/mnt/user/appdata/pihole-dot-doh/pihole/'
+dnsmasq_volume='vdnsmasq' #'/mnt/user/appdata/pihole-dot-doh/dnsmasq.d/'
+config_volume='vconfig' #'/mnt/user/appdata/pihole-dot-doh/config/'
+
+docker run -d \
+    --name='pihole-dot-doh' \
+    --cap-add=NET_ADMIN \
+    --restart=unless-stopped \
+    --net='bridge' \
+    -e TZ="Europe/London" \
+    -e HOST_OS="Unraid" \
+    -v $pihole_volume:'/etc/pihole/':'rw' \
+    -v $dnsmasq_volume:'/etc/dnsmasq.d/':'rw' \
+    -v $config_volume:'/config':'rw' \
+    -e 'DNS1'='127.1.1.1#5153' \
+    -e 'DNS2'='127.2.2.2#5253' \
+    -e 'TZ'='Europe/London' \
+    -e 'WEBPASSWORD'='password' \
+    -e 'INTERFACE'=$interface \
+    -e 'ServerIP'=$ipAddress \
+    -e 'ServerIPv6'='' \
+    -e 'IPv6'='False' \
+    -e 'DNSMASQ_LISTENING'='all' \
+    -p '10053:53/tcp' \
+    -p '10053:53/udp' \
+    -p '10067:67/udp' \
+    -p '10080:80/tcp' \
+    -p '10443:443/tcp' \
+    'richy1989/pihole-dot-doh:latest'
