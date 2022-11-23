b97706abe6f9be1439fb73b4293d86b3ad98dfcc
Richard Leopold <richy.leopold@outlook.com>
2022-11-23 22:34:27 +0100

    seperated services

--- README.md
+++ README.md
@@ -44,6 +44,6 @@ For docker parameters, refer to [official pihole docker readme](https://github.c
   * Cloudflared client was obtained from [official site](https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/installation#linux)
   * Stubby is a standard debian package
 * I code for fun and my personal uses; hence, these niche functionalties that nobody asks for. ;)
-* If you like my work, [a donation to my burger fund](https://paypal.me/mersenne) is very much appreciated.
+* If you like my work, [a donation to my burger fund](-----) is very much appreciated.
 
-[![Donate](https://raw.githubusercontent.com/testdasi/testdasi-unraid-repo/master/donate-button-small.png)](https://paypal.me/mersenne). 
+[![Donate](https://raw.githubusercontent.com/testdasi/testdasi-unraid-repo/master/donate-button-small.png)](----). 
--- stuff/install_configs.sh
+++ stuff/install_configs.sh
@@ -2,39 +2,53 @@
 
 # Creating pihole-dot-doh service
 
-serviceFile='pihole-dot-doh'
+serviceFileStubby='pihole-dot-doh-stubby'
+serviceFileCloudfared='pihole-dot-doh-cloudf'
 servicePath='/etc/s6-overlay/s6-rc.d/' #'/etc/services.d/'
 
-fullServicePath=$servicePath$serviceFile
+fullServicePathStubby=$servicePath$serviceFileStubby
+fullServicePathCloudfared=$servicePath$serviceFileCloudfared
 
+mkdir -p $fullServicePathStubby
+mkdir -p $fullServicePathCloudfared
 
-
-mkdir -p $fullServicePath
-
+#Stubby
 # run file
-echo '#!/command/with-contenv bash' > $fullServicePath'/run'
+echo '#!/command/with-contenv bash' > $fullServicePathStubby'/run'
 # Copy config file if not exists
-echo 'cp -n /temp/stubby.yml /config/' >> $fullServicePath'/run'
-echo 'cp -n /temp/cloudflared.yml /config/' >> $fullServicePath'/run'
+echo 'cp -n /temp/stubby.yml /config/' >> $fullServicePathStubby'/run'
 # run stubby in background
-echo 's6-echo "Starting stubby"' >> $fullServicePath'/run'
-echo 'stubby -g -C /config/stubby.yml' >> $fullServicePath'/run'
-# run cloudflared in foreground
-echo 's6-echo "Starting cloudflared"' >> $fullServicePath'/run'
-echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> $fullServicePath'/run'
+echo 's6-echo "Starting stubby"' >> $fullServicePathStubby'/run'
+echo 'stubby -g -C /config/stubby.yml' >> $fullServicePathStubby'/run'
 
 # finish file
-echo '#!/command/with-contenv bash' > $fullServicePath'/finish'
-echo 's6-echo "Stopping stubby"' >> $fullServicePath'/finish'
-echo 'killall -9 stubby' >> $fullServicePath'/finish'
-echo 's6-echo "Stopping cloudflared"' >> $fullServicePath'/finish'
-echo 'killall -9 cloudflared' >> $fullServicePath'/finish'
+echo '#!/command/with-contenv bash' > $fullServicePathStubby'/finish'
+echo 's6-echo "Stopping stubby"' >> $fullServicePathStubby'/finish'
+echo 'killall -9 stubby' >> $fullServicePathStubby'/finish'
 
 #type file
-echo 'longrun' >> $fullServicePath'/type'
+echo 'longrun' >> $fullServicePathStubby'/type'
+
+#CLOUDFARED
+# run file
+echo '#!/command/with-contenv bash' > $fullServicePathCloudfared'/run'
+# Copy config file if not exists
+echo 'cp -n /temp/cloudflared.yml /config/' >> $fullServicePathCloudfared'/run'
+# run cloudflared in foreground
+echo 's6-echo "Starting cloudflared"' >> $fullServicePathCloudfared'/run'
+echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> $fullServicePathCloudfared'/run'
+
+# finish file
+echo '#!/command/with-contenv bash' > $fullServicePathCloudfared'/finish'
+echo 's6-echo "Stopping cloudflared"' >> $fullServicePathCloudfared'/finish'
+echo 'killall -9 cloudflared' >> $fullServicePathCloudfared'/finish'
+
+#type file
+echo 'longrun' >> $fullServicePathCloudfared'/type'
 
 #contents file 
-echo '' >> $servicePath'user/contents.d/'$serviceFile
+echo '' >> $servicePath'user/contents.d/'$serviceFileCloudfared
+echo '' >> $servicePath'user/contents.d/'$serviceFileStubby
 
 # - !/command/with-contenv bash
 # - !/usr/bin/with-contenv bash'
