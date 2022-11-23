206416fca2e9e79758e66539ed166dd416d07107
Richard Leopold <richy.leopold@outlook.com>
2022-11-23 15:31:58 +0100

    new service path

--- stuff/install_configs.sh
+++ stuff/install_configs.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 # Creating pihole-dot-doh service
-mkdir -p /etc/services.d/pihole-dot-doh
+
+serviceFile='pihole-dot-doh'
+servicePath='/etc/s6-overlay/s6-rc.d/' #'/etc/services.d/'
+
+fullServicePath=$servicePath$serviceFile
+
+mkdir -p $fullServicePath
 
 # run file
-echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/run
+echo '#!/command/with-contenv bash' > /etc/services.d/pihole-dot-doh'/run'
 # Copy config file if not exists
-echo 'cp -n /temp/stubby.yml /config/' >> /etc/services.d/pihole-dot-doh/run
-echo 'cp -n /temp/cloudflared.yml /config/' >> /etc/services.d/pihole-dot-doh/run
+echo 'cp -n /temp/stubby.yml /config/' >> $fullServicePath'/run'
+echo 'cp -n /temp/cloudflared.yml /config/' >> $fullServicePath'/run'
 # run stubby in background
-echo 's6-echo "Starting stubby"' >> /etc/services.d/pihole-dot-doh/run
-echo 'stubby -g -C /config/stubby.yml' >> /etc/services.d/pihole-dot-doh/run
+echo 's6-echo "Starting stubby"' >> $fullServicePath'/run'
+echo 'stubby -g -C /config/stubby.yml' >> $fullServicePath'/run'
 # run cloudflared in foreground
-echo 's6-echo "Starting cloudflared"' >> /etc/services.d/pihole-dot-doh/run
-echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> /etc/services.d/pihole-dot-doh/run
+echo 's6-echo "Starting cloudflared"' >> $fullServicePath'/run'
+echo '/usr/local/bin/cloudflared --config /config/cloudflared.yml' >> $fullServicePath'/run'
 
 # finish file
-echo '#!/usr/bin/with-contenv bash' > /etc/services.d/pihole-dot-doh/finish
-echo 's6-echo "Stopping stubby"' >> /etc/services.d/pihole-dot-doh/finish
-echo 'killall -9 stubby' >> /etc/services.d/pihole-dot-doh/finish
-echo 's6-echo "Stopping cloudflared"' >> /etc/services.d/pihole-dot-doh/finish
-echo 'killall -9 cloudflared' >> /etc/services.d/pihole-dot-doh/finish
+echo '#!/command/with-contenv bash' > $fullServicePath'/finish'
+echo 's6-echo "Stopping stubby"' >> $fullServicePath'/finish'
+echo 'killall -9 stubby' >> $fullServicePath'/finish'
+echo 's6-echo "Stopping cloudflared"' >> $fullServicePath'/finish'
+echo 'killall -9 cloudflared' >> $fullServicePath'/finish'
+
+
+# - !/command/with-contenv bash
+# - !/usr/bin/with-contenv bash'
