8bfbef8719bf08d7508f4f49f6e60631a9a37798
Richard Leopold <richy.leopold@outlook.com>
2022-11-23 16:42:00 +0100

    removed other plattforms

--- stuff/install.sh
+++ stuff/install.sh
@@ -16,34 +16,13 @@ mkdir -p /etc/stubby \
 # install cloudflared
 mkdir -p /tmp \
     && cd /tmp
-if [[ ${TARGETPLATFORM} =~ "arm64" ]]
-then
-    curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb -o /tmp/cloudflared.deb
-    dpkg --add-architecture arm64
-    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
-elif [[ ${TARGETPLATFORM} =~ "amd64" ]]
-then 
-    curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o /tmp/cloudflared.deb
+
+curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb -o /tmp/cloudflared.deb
     dpkg --add-architecture amd64
     echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
-elif [[ ${TARGETPLATFORM} =~ "386" ]]
-then
-    curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386.deb -o /tmp/cloudflared.deb
-    dpkg --add-architecture 386
-    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
-elif [[ ${TARGETPLATFORM} =~ 'arm/v7' ]]
-then
-    curl -sL https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb -o /tmp/cloudflared.deb
-    dpkg --add-architecture arm
-    echo "$(date "+%d.%m.%Y %T") Added cloudflared for ${TARGETPLATFORM}" >> /build.info
-#elif [[ ${TARGETPLATFORM} =~ 'arm/v6' ]]
-#then
-#    curl -sL https://hobin.ca/cloudflared/releases/2022.3.1/cloudflared_2022.3.1_arm.deb -o /tmp/cloudflared.deb
-else 
-    echo "Target Plattform:"${TARGETPLATFORM} >> /build.info
-    echo "$(date "+%d.%m.%Y %T") Unsupported platform - cloudflared not added" >> /build.info
-fi
-apt install /tmp/cloudflared.deb \
+    
+#apt install /tmp/cloudflared.deb \
+dpkg -i /tmp/cloudflared.deb \
     && rm -f /tmp/cloudflared.deb \
     && useradd -s /usr/sbin/nologin -r -M cloudflared \
     && chown cloudflared:cloudflared /usr/local/bin/cloudflared
